502f7093382c3c578b567bb29b8175511e21dc8e
Sheena Bonilla <sheena.myb@gmail.com>
2022-08-29 11:36:14 -0700

    commit tree cdf313124bfc11ad2be2aec866081189177b7e86

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,10 +2,27 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
+
   render() {
+
+   const {reviews, restaurantId, deleteReview} = this.props;
+   debugger
+   const associatedReviews = reviews.filter((review) => review.restaurantId === restaurantId)
+
+   const reviewsList = associatedReviews.map((review) => {
+    return (
+      <Review 
+        key={review.id}
+        review={review}
+        deleteReview={deleteReview}
+      />
+    )
+  });
+
     return (
       <ul>
         Reviews
+        {reviewsList}
       </ul>
     );
   }
